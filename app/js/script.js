3a4abc76b2a578ce5627462438071bef3c1e695d
PavankumarKokane <pavankokane2013@gmail.com>
2024-05-29 11:03:24 +0530

    validation added

--- app/js/script.js
+++ app/js/script.js
@@ -15,6 +15,13 @@
       $(this).addClass("active");
     });
 
+    $('.scroll-sec').click(function(){
+      var scoll_sec = $(this).attr('rel');
+      var sec_top = $('#'+scoll_sec).offset().top - 100;
+      var scrollto =  sec_top;
+      $('html, body').animate({scrollTop:scrollto}, 100);
+    });
+
     function slider_initial(sliderClass) {
       $("." + sliderClass).slick({
         dots: false,
@@ -59,5 +66,129 @@
       $(".lang-div a").removeClass("active");
       $(this).addClass("active");
     });
+
+    // Contact Form validations
+
+    $("#inquiry-form input").attr("autocomplete", "one-time-code");
+    $("#inquiry-form input").on("copy paste", function (e) {
+      e.preventDefault();
+    });
+
+    const alphaOnly = document.querySelectorAll(".alpha-only");
+    alphaOnly.forEach(function (element) {
+      element.addEventListener("beforeinput", function (event) {
+        if (event.inputType === "deleteContentBackward") {
+          return;
+        }
+        var value = this.value;
+        if (
+          !/^[a-zA-Z ]$/.test(event.data) ||
+          (event.data === " " && value.length === 0)
+        ) {
+          event.preventDefault();
+        }
+      });
+    });
+
+    const numericOnly = document.querySelectorAll(".numeric-only");
+    numericOnly.forEach(function (element) {
+      element.addEventListener("beforeinput", function (event) {
+        // Check if the input is the backspace key
+        if (event.inputType === "deleteContentBackward") {
+          // If it is, allow the input
+          return;
+        }
+
+        // Check if the input is not a number
+        if (!/^\d$/.test(event.data)) {
+          // If it's not allowed, cancel the input event
+          event.preventDefault();
+        }
+      });
+    });
+
+    // Function to set a cookie
+    function setCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    // Function to get a cookie
+    function getCookie(name) {
+      var nameEQ = name + "=";
+      var cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == " ") {
+          cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) == 0) {
+          return cookie.substring(nameEQ.length, cookie.length);
+        }
+      }
+      return null;
+    }
+
+    // Function to parse URL parameters
+    function getUrlParams(url) {
+      var params = {};
+      var parser = document.createElement("a");
+      parser.href = url;
+      var query = parser.search.substring(1);
+      var vars = query.split("&");
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+      }
+      return params;
+    }
+
+    // Function to set UTM parameters as cookies
+    function setUTMCookies() {
+      var urlParams = getUrlParams(window.location.href);
+      var utmSource = urlParams["utm_source"];
+      var utmMedium = urlParams["utm_medium"];
+      var utmCampaign = urlParams["utm_campaign"];
+      var utmTerm = urlParams["utm_term"];
+      var utmContent = urlParams["utm_content"];
+
+      if (utmSource) {
+        setCookie("utm_new_source", utmSource, 7);
+      }
+      if (utmMedium) {
+        setCookie("utm_new_medium", utmMedium, 7);
+      }
+      if (utmCampaign) {
+        setCookie("utm_new_campaign", utmCampaign, 7);
+      }
+      if (utmTerm) {
+        setCookie("utm_new_term", utmTerm, 7);
+      }
+      if (utmContent) {
+        setCookie("utm_new_content", utmContent, 7);
+      }
+    }
+
+    var utmSource = getCookie("utm_new_source");
+
+    console.log(utmSource, "What is this");
+
+    if (utmSource === null || utmSource == "") {
+      setUTMCookies();
+      console.log("UTM Cookies set");
+    } else {
+      console.log("UTM Cookies already set");
+    }
+
+    var utmSource = getCookie("utm_new_source");
+    var utmCampaign = getCookie("utm_new_campaign");
+    var utmMedium = getCookie("utm_new_medium");
+    var utmTerm = getCookie("utm_new_term");
+    console.log(utmSource, utmCampaign, utmMedium, utmTerm);
   });
 }.call(window, window.jQuery);
